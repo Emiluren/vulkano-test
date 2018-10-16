92b5c814316a797934843440cb1115e52d3a27b5
Emil Segerbäck <emilsegerback@gmail.com>
2018-10-16 16:50:58 +0200

    Finish mandelbrot renderer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-#[macro_use]
+extern crate image;
 extern crate vulkano;
+#[macro_use]
+extern crate vulkano_shader_derive;
 
+use std::sync::Arc;
+
+use image::{ImageBuffer, Rgba};
+
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{CommandBuffer, AutoCommandBufferBuilder};
+use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::device::{Device, DeviceExtensions};
+use vulkano::format::Format;
+use vulkano::image::{Dimensions, StorageImage};
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
-use vulkano::device::Device;
-use vulkano::device::DeviceExtensions;
 use vulkano::instance::Features;
-use vulkano::buffer::BufferUsage;
-use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::pipeline::ComputePipeline;
+use vulkano::sync::GpuFuture;
+
+mod compiled_shader {
+    #[derive(VulkanoShader)]
+    #[ty = "compute"]
+    #[path = "src/mandelbrot.glsl"]
+    struct Dummy;
+}
 
 fn main() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
@@ -32,12 +49,42 @@ fn main() {
 
     let queue = queues.next().unwrap();
 
-    struct MyStruct {
-        a: u32,
-        b: bool,
-    }
+    let image = StorageImage::new(
+        device.clone(),
+        Dimensions::Dim2d { width: 1024, height: 1024 },
+        Format::R8G8B8A8Unorm,
+        Some(queue.family())
+    ).unwrap();
 
-    let data = MyStruct { a: 5, b: true };
-    let buffer = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(),
-                                                data).expect("failed to create buffer");
+    let shader = compiled_shader::Shader::load(device.clone())
+        .expect("failed to create shader module");
+
+    let compute_pipeline = Arc::new(ComputePipeline::new(
+        device.clone(),
+        &shader.main_entry_point(),
+        &()
+    ).expect("failed to create compute pipeline"));
+
+    let set = Arc::new(PersistentDescriptorSet::start(compute_pipeline.clone(), 0)
+                       .add_image(image.clone()).unwrap()
+                       .build().unwrap()
+    );
+
+    let buf = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        (0 .. 1024 * 1024 * 4)
+            .map(|_| 0u8)).expect("failed to create buffer");
+
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
+        .dispatch([1024 / 8, 1024 / 8, 1], compute_pipeline.clone(), set, ()).unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap()
+        .build().unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap().wait(None).unwrap();
+
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("image.png").unwrap();
 }
